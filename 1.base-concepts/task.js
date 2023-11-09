397f4d40a17ba3c26b989f689288e032f907f432
orbb2010 <orbb2010@inbox.ru>
2023-11-10 01:00:13 +0500

    Initial commit. 1 task in process

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,16 @@
-"use strict"
+"use strict";
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let diskriminant;
+  diskriminant = b**2 - 4*a*c;
+  if diskriminant < 0 {
+    arr = [];
+  } else if diskriminant = 0 {
+    arr[0] = -b / (2*a);
+  } else {
+    arr[0] = (-b + Math.sqrt(d) )/(2*a);
+    arr[1] = (-b - Math.sqrt(d) )/(2*a);
+  }
   return arr;
 }
 
