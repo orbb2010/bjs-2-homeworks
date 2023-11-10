9f18441863486999911182242f509fe85c4bfa51
Orbb2010 <orbb2010@inbox.ru>
2023-11-10 12:58:58 +0500

    1 task ready

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,15 +1,15 @@
 "use strict";
 function solveEquation(a, b, c) {
   let arr = [];
-  let diskriminant;
-  diskriminant = b**2 - 4*a*c;
-  if diskriminant < 0 {
+  let d;
+  d = b ** 2 - 4 * a * c;
+  if (d < 0) {
     arr = [];
-  } else if diskriminant = 0 {
-    arr[0] = -b / (2*a);
+  } else if (d === 0) {
+    arr[0] = - b / (2 * a);
   } else {
-    arr[0] = (-b + Math.sqrt(d) )/(2*a);
-    arr[1] = (-b - Math.sqrt(d) )/(2*a);
+    arr[0] = (- b + Math.sqrt(d) )/(2 * a);
+    arr[1] = (- b - Math.sqrt(d) )/(2 * a);
   }
   return arr;
 }
