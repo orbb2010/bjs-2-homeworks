2f9c708a88a450a3a57f2636339560be52251524
Orbb2010 <orbb2010@inbox.ru>
2023-11-17 14:27:16 +0500

    READY, SUCCESS ! Thanks!

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -87,7 +87,7 @@ function averageEvenElementsWorker(...arr) {
 function makeWork (arrOfArr, func) {
   let maxWorkerResult = -Infinity;
   for (let i = 0; i < arrOfArr.length; i++) {
-    let funcResult = func(...arrOfArr);
+    let funcResult = func(...arrOfArr[i]);
     if (funcResult > maxWorkerResult) {
       maxWorkerResult = funcResult;
     } 
