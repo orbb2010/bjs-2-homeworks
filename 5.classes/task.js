23ea2ed40f72613af95d6c3bb304175a25b1afaf
orbb2010 <orbb2010@inbox.ru>
2023-12-07 00:06:52 +0500

    06.12.2023 updated

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -75,23 +75,22 @@ class Library{
 
   findBookBy(type, value){
     for (let i = 0; i < this.books.length; i++) {
-      if (this.books[i].hasOwnProperty(type) && this.books[i][type] === value) {
-        return this.books[i];
-      } else {
-        return null;
+      if (this.books[i].hasOwnProperty(type)) {
+        if (this.books[i][type] === value) {
+          return this.books[i];         
       }
-    }
-  }  
+    }      
+  }
+  return null;
+}
 
   giveBookByName(bookName){
-    if (this.findBookBy(bookName) !== bookName) {
-        return null;
+    for (let i = 0; i < this.books.length; i++) {
+      if (this.books[i].name === bookName) {
+        return this.books[i];
       } else {
-        this.books.filter(function (bookName) {
-          if (this.findBookBy(bookName) === bookName) {
-            return this.findBookBy(bookName);
-          }
-          });
-        }   
-  }
+          return this.books.splice(i, -1);     
+        }
+    }  
+  }  
 }
